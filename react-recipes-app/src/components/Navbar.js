7825716101eb3c11a951a2ba90d284e555d3b3c4
tatizapi <tatiana.zapirtan@yahoo.com>
2020-12-31 20:52:44 +0200

    added top navbar

---
+++ react-recipes-app/src/components/Navbar.js
@@ -0,0 +1,27 @@
+import './css/Navbar.css';
+import { GiCookingPot } from 'react-icons/gi';
+import { IconContext } from "react-icons";
+
+
+const Navbar = () => {
+    return (
+        <div className="navbar">
+            <div>
+                <IconContext.Provider value={{ color: 'white', size: '60px', className: "navbar-home-logo" }}>
+                    <GiCookingPot />
+                </IconContext.Provider>
+                <div className="navbar-home-title"><a href='/'>Recipes</a></div>
+            </div>
+            <div className="navbar-food-categories">
+                <ul>
+                    <li><a href='/'>Soups</a></li>
+                    <li><a href='/'>Meat</a></li>
+                    <li><a href='/'>Pasta</a></li>
+                    <li><a href='/'>Desert</a></li>
+                </ul>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
